005b93af6c2584a385f26d077de35d70d7137c73
Vlad Hrushovyi <vlad.grush.99@gmail.com>
2023-03-06 18:06:00 +0200

    try array and index expr

---
+++ IndexExpr__Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Expressions;
+
+namespace Expression__Test;
+
+public class IndexExpr__Test
+{
+    public void Main()
+    {
+        var arrayExpr = Expression.Parameter(typeof(int[]), "array");
+        var indexExpr = Expression.Parameter(typeof(int), "index");
+        var valueExpr = Expression.Parameter(typeof(int), "value");
+
+        var arrAccessExpr = Expression.ArrayAccess(
+                arrayExpr,
+                indexExpr
+            );
+
+        var lambda = Expression.Lambda<Func<int[], int, int, int>>(
+            Expression.Assign(
+                arrAccessExpr, Expression.Add(arrAccessExpr, valueExpr)),
+            arrayExpr,
+            indexExpr,
+            valueExpr
+        ).Compile();
+        
+        Console.WriteLine(lambda(new int[]{ 1, 2, 3, 4, 5 }, 3, 10));
+    }
+}
